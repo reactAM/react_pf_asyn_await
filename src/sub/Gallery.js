3a7a983bf9836680e6fca316cf32138d91cb8df0
tubi55 <tubi55@nate.com>
2021-12-27 12:10:01 +0900

    async awit로 동기적으로 컨텐츠 로딩이 완료되면 list출력 모션 추가

--- src/sub/Gallery.js
+++ src/sub/Gallery.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+//useRef import
+import { useEffect, useState, useRef } from "react";
 
 function Gallery(){
   const baseURL = "https://www.flickr.com/services/rest/?";
@@ -7,26 +8,22 @@ function Gallery(){
   const key= "e7ed3b39fe112d7e93d03c19325305e0";
   const count = 500;
   const url = `${baseURL}method=${method1}&api_key=${key}&per_page=${count}&format=json&nojsoncallback=1`;
-
-  //flickr데이터 배열을 받을 빈 배열을 items이름의 state로 설정
+  
   let [items, setItems] = useState([]);
-
-  //useEffect의 의존성을 비워서 처음 로딩시 한번만 실행되게 설정
+  //list를 참조하기 위해 일단 useRef에 null을 담아 list변수 생성
+  let list = useRef(null);
+  console.log(list);
+  
   useEffect(()=>{
-    axios
-    .get(url)
-    .then(json=>{
-      console.log(json.data.photos.photo);
-      setItems(json.data.photos.photo);
-    })
+    getFlickr();
   },[]);
 
   return (
     <section className="content gallery">
       <div className="inner">
         <h1>Gallery</h1>
-
-        <ul className="list">
+        {/* 참고 싶은 요소에 ref props를 만들고 변수 list대입 */}
+        <ul className="list" ref={list}>
           {
             items.map((item, index)=>{
               const imgSrc = `https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`;
@@ -45,6 +42,22 @@ function Gallery(){
       </div>
     </section>
   )
+
+  //wrapping함수에 async 키워드 적용
+  async function getFlickr(){
+
+    //axio문 앞쪽에 await키워드를 추가해서 동기화
+    await axios
+    .get(url)
+    .then(json=>{      
+      setItems(json.data.photos.photo);
+    })
+    
+    //위쪽의 axios를 통해서 데이터가 모두 호출완료되면 list에 on을 붙여서 로딩 모션 추가
+    list.current.classList.add("on"); 
+  }
 }
+
+
 
 export default Gallery;
