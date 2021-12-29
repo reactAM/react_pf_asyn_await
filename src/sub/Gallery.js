342e11d88e7c0486304788887b8173ac34dd03c4
tubi55 <tubi55@nate.com>
2021-12-29 10:32:05 +0900

    모션중 재이벤트 방지

--- src/sub/Gallery.js
+++ src/sub/Gallery.js
@@ -2,10 +2,8 @@ import axios from "axios";
 import { useEffect, useState, useRef } from "react";
 import Masonry from "react-masonry-component";
 
-//public폴더의 절대경로를 변수에 저장
 const path=process.env.PUBLIC_URL;
 
-//masonry 개별 패널 옵션
 const masonryOptions = {
   fitWidth: false, 
   //columnWidth: 200,
@@ -14,9 +12,10 @@ const masonryOptions = {
 }
 
 function Gallery(){ 
-  let [items, setItems] = useState([]); 
-  //로딩바 출력 유무를 결정할 state생성하고 초기값을 true로 설정
+  let [items, setItems] = useState([]);   
   let [loading, setLoading] = useState(true);
+  //enableClick에 true값 저장
+  let [enableClick, setEnableClick] = useState(true);
   let list = useRef(null);  
   let [url, url2] = getURL();  
   
@@ -28,20 +27,31 @@ function Gallery(){
     <section className="content gallery">
       <div className="inner">
         <h1 onClick={()=>{
-          list.current.classList.remove("on");
-          //기존 리스트가 아래로 사라질떄 다시 loading값 true로 변경
-          setLoading(true);
-          getFlickr(url);
+          //이벤트 안쪽 코드를 enableClick이 true일때만 실행
+          if(enableClick){
+            //조건식을 통과하자마자 enableClick값을 바로 false로 변경
+            setEnableClick(false);
+            console.log("You Clicked!!!")
+            list.current.classList.remove("on");         
+            setLoading(true);
+            getFlickr(url);
+          }
+          
         }}>Gallery</h1>
        
-        <button onClick={()=>{          
-          list.current.classList.remove("on");   
-          //기존 리스트가 아래로 사라질떄 다시 loading값 true로 변경
-          setLoading(true);       
-          getFlickr(url2);
+        <button onClick={()=>{   
+          //이벤트 안쪽 코드를 enableClick이 true일때만 실행
+          if(enableClick){
+            //조건식을 통과하자마자 enableClick값을 바로 false로 변경
+            setEnableClick(false);
+            console.log("You Clicked!!!")
+            list.current.classList.remove("on"); 
+            setLoading(true);       
+            getFlickr(url2);
+          }  
         }}>수정</button>
 
-        {/* 삼항 연산자로 loading state값이 true일 때에만 로딩이미지 출력 */}
+       
         {(loading) ? <img className="loading" src={path+"/img/loading.gif"}/> : ""}
        
         <div className="list" ref={list}>         
@@ -90,12 +100,14 @@ function Gallery(){
       setItems(json.data.photos.photo);
     })    
     
-    //masonry ui가 적용될 시간을 벌기 위해서 1초뒤 리스트 활성화
+   
     setTimeout(()=>{
-      list.current.classList.add("on");
-      //데이터 호출이 끝나면 setLoading으로 loading state값 false로 변경
+      list.current.classList.add("on");      
       setLoading(false);
-    },1000);
+      setTimeout(()=>{
+        setEnableClick(true);
+      },1000); //list에 on이 붙어서 올라오는 모션동안 방지
+    },1000); //masonry ui가 적용되는 시간동안 방지
      
   }
 }
